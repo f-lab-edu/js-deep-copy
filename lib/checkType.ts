e5f0d323ec49e5f1f57611086a0ac8fd188ec5c6
Jung Su <wjdwjdtn92@naver.com>
2024-02-09 23:53:35 +0900

    Feat: checkType.ts 추가

---
+++ lib/checkType.ts
@@ -0,0 +1,27 @@
+export function isNull(value: any): value is null {
+  return value === null
+}
+
+export function isMap(value: any): value is Map<any, any> {
+  return value instanceof Map
+}
+
+export function isSet(value: any): value is Set<any> {
+  return value instanceof Set
+}
+
+export function isDate(value: any): value is Date {
+  return value instanceof Date
+}
+
+export function isRegExp(value: any): value is RegExp {
+  return value instanceof RegExp
+}
+
+export function isFunction(value: any): value is Function {
+  return typeof value === 'function'
+}
+
+export function isObject(value: any): value is Object {
+  return typeof value === 'object'
+}
